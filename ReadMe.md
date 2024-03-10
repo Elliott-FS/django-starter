0071728125c30fa9ebb45b3a25263e156abec2e7
Elliott Delano Wynn III <delanowynn@yahoo.com>
2024-03-10 00:27:55 -0500

    added read me/paths for template render/app working

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# First Django project! Excited to get started.
+# to run file run python3 manage.py runserver in parent mydjangostarter file
--- mydjangostarter/settings.py
+++ mydjangostarter/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mydjangostarter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR), "myapp/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
